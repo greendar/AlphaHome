aa135c79323f3205aafe91840a13233ddfdbedd3
Mr Green <dwgreen@hwdsb.on.ca>
2020-03-10 11:03:32 -0400

    Created functions for dot product and cross product. Changed Vector method show to __str__ and modified it to detect where the vector is 2d or 3d.

--- vectors.py
+++ vectors.py
@@ -1,13 +1,27 @@
+def dot(aVec, bVec):
+    return aVec.vx * bVec.vx + aVec.vy * bVec.vy + aVec.vz * bVec.vz
+
+def cross(aVec, bVec):
+    cX = aVec.vy * bVec.vz - aVec.vz - bVec.vy
+    cY = aVec.vz * bVec.vx - aVec.vx - bVec.vz
+    cZ = aVec.vx * bVec.vy - aVec.vy - bVec.vx
+    c = Vector(cX, cY, cZ)
+    return c
+    
 class Vector:
-    def __init__(self, vx, vy):
+    def __init__(self, vx, vy, vz = 0):
         self.vx = vx
         self.vy = vy
+        self.vz = vz
 
-    def show(self):
-        print(f"[{self.vx}, {self.vy}]")
+    def __str__(self):
+        if self.vz == 0:
+            print(f"[{self.vx}, {self.vy}]")
+        else:
+            print(f"[{self.vx}, {self.vy}, {self.vz}]")
 
     def mag(self):
-        return (self.vx**2 + self.vy**2)**0.5
+        return (self.vx**2 + self.vy**2 +self.vz**2)**0.5
 
 
 """
