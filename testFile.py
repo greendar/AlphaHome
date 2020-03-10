71b140d51f46edf46115606cd5e54d176a354d53
Mr Green <dwgreen@hwdsb.on.ca>
2020-03-10 18:26:40 -0400

    created an __add__ operator overload. updated testFile to test out new operations fixed error in cross method

--- testFile.py
+++ testFile.py
@@ -1,6 +1,13 @@
-import vectors as v
+from vectors import *
 
-a = v.Vector(3,4)
+a = Vector(3,4)
+b = Vector(1,2)
+c = Vector(5,6,7)
 
 print(a.mag())
 print(a)
+print(c)
+
+print(dot(a,b))
+print(cross(a,b))
+print(a+b)
--- vectors.py
+++ vectors.py
@@ -2,9 +2,9 @@ def dot(aVec, bVec):
     return aVec.vx * bVec.vx + aVec.vy * bVec.vy + aVec.vz * bVec.vz
 
 def cross(aVec, bVec):
-    cX = aVec.vy * bVec.vz - aVec.vz - bVec.vy
-    cY = aVec.vz * bVec.vx - aVec.vx - bVec.vz
-    cZ = aVec.vx * bVec.vy - aVec.vy - bVec.vx
+    cX = aVec.vy * bVec.vz - aVec.vz * bVec.vy
+    cY = aVec.vz * bVec.vx - aVec.vx * bVec.vz
+    cZ = aVec.vx * bVec.vy - aVec.vy * bVec.vx
     c = Vector(cX, cY, cZ)
     return c
 
@@ -20,16 +20,13 @@ class Vector:
         else:
             return f"[{self.vx}, {self.vy}, {self.vz}]"
 
+    def __add__(self, other):
+        cX = self.vx + other.vx
+        cY = self.vy + other.vy
+        cZ = self.vz + other.vz
+        c = Vector(cX, cY, cZ)
+        return c
+
+
     def mag(self):
         return (self.vx**2 + self.vy**2 +self.vz**2)**0.5
-
-
-"""
-a = Vector(3, 4)
-b = Vector(5, 6)
-
-a.show()
-b.show()
-
-
-"""
