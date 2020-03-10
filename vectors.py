7f86600373d13d580a96dbc157772a036b677699
Mr Green <dwgreen@hwdsb.on.ca>
2020-03-10 07:46:30 -0400

    Initial Upload
    Vector File added

---
+++ vectors.py
@@ -0,0 +1,19 @@
+class Vector:
+    def __init__(self, vx, vy):
+        self.vx = vx
+        self.vy = vy
+
+    def show(self):
+        print(f"[{self.vx}, {self.vy}]")
+
+a = Vector(3, 4)
+b = Vector(5, 6)
+
+a.show()
+b.show()
+
+
+
+
+
+    
